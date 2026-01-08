73f397e76ce2deb92a17f54674e51724aeceda94
Aleksandr Shuvalov"git config --global user.email 2322972@gmail.comgit config --listgit config--listgit config --global user.name Aleksandr <2322972@gmail.com>
2026-01-08 18:27:54 +0300

    добавлен файл datamart_query.sql

---
+++ project/task1/datamart_query.sql
@@ -0,0 +1,19 @@
+insert into dm_rfm_segments(user_id, recency, frequency, monetary_value)
+
+select trr.user_id,trr.recency,trf.frequency,trmv.monetary_value
+from
+tmp_rfm_recency trr join tmp_rfm_frequency trf on trr.user_id = trf.user_id
+join tmp_rfm_monetary_value trmv on trr.user_id = trmv.user_id
+
+|user_id|recency|frequency|monetary_value|
+|-------|-------|---------|--------------|
+|0      |1      |3        |4             |
+|1      |4      |3        |3             |
+|2      |2      |3        |5             |
+|3      |2      |3        |3             |
+|4      |4      |3        |3             |
+|5      |5      |5        |5             |
+|6      |1      |3        |5             |
+|7      |4      |2        |2             |
+|8      |1      |2        |3             |
+|9      |1      |3        |2             |
